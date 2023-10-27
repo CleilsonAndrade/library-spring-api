8aedab790b53388c75088c9b042f01e22e372fb5
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-10-27 10:40:17 -0300

    User does not exist exception

--- src/main/java/br/cleilsonandrade/libraryspringapi/controller/exception/ControllerException.java
+++ src/main/java/br/cleilsonandrade/libraryspringapi/controller/exception/ControllerException.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.cleilsonandrade.libraryspringapi.service.exception.EntityNotFound;
 import br.cleilsonandrade.libraryspringapi.service.exception.UserAlreadyExists;
+import br.cleilsonandrade.libraryspringapi.service.exception.UserDoesNotExist;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
@@ -30,6 +31,18 @@ public class ControllerException {
       HttpServletRequest request) {
     String error = "User already exists";
     HttpStatus status = HttpStatus.CONFLICT;
+    ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, entityNotFound.getMessage(),
+        request.getRequestURI());
+    request.getRequestURI();
+
+    return ResponseEntity.status(status).body(err);
+  }
+
+  @ExceptionHandler(EntityNotFound.class)
+  public ResponseEntity<ErrorResponse> userDoesNotExistError(UserDoesNotExist entityNotFound,
+      HttpServletRequest request) {
+    String error = "User does not exist";
+    HttpStatus status = HttpStatus.NOT_FOUND;
     ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, entityNotFound.getMessage(),
         request.getRequestURI());
     request.getRequestURI();
